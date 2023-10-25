cbf660645ad9b8aeacefc157e701759d42afe86e
fufupatwo <114861274+fufupatwo@users.noreply.github.com>
2023-10-25 05:16:41 +0000

    -Finished main form validation
    -Next up confirming emails

--- index.js
+++ index.js
@@ -43,53 +43,45 @@ document.addEventListener("DOMContentLoaded", function(){
   }
 
 
+  const validateForm = () => {
+
+    let containsErrors = false;
+
+    var petitionInputs = document.getElementById("sign-petition").elements;
+    // TODO: Loop through all inputs
+    for(let i =0; i < petitionInputs.length; i++)
+      {
+          if(petitionInputs[i].value.length < 2)
+          {
+            petitionInputs[i].classList.add('error');
+            containsErrors = true;
+          }
+          else
+          {
+            petitionInputs[i].classList.remove('error');
+          }
+
+      }
+
+    // TODO: Validate the value of each input
+    if(containsErrors == false)
+    {
+      addSignature();
+      for(let i =0; i < petitionInputs.length; i++)
+        {
+          petitionInputs[i].value = "";
+          containsErrors = false;
+        }
+    }
+
+    // TODO: Call addSignature() and clear fields if no errors
+
+  }
+
+  signNowButton.addEventListener('click', validateForm);
 
   // Add a click event listener to the sign now button here
 });
-
-
-
-// TODO: Remove the click event listener that calls addSignature()
-
-// TODO: Complete validation form
-
-const validateForm = () => {
-
-  let containsErrors = false;
-
-  var petitionInputs = document.getElementById("sign-petition").elements;
-  // TODO: Loop through all inputs
-  for(let i =0; i < petitionInputs; i++)
-    {
-        if(petitionInputs[i].value.length < 2)
-        {
-          petitionInputs[i].classList.add('error');
-          containsErrors = true;
-        }
-        else
-        {
-          petitionInputs[i].classList.remove('error');
-        }
-      
-    }
-
-  // TODO: Validate the value of each input
-
-
-
-  // TODO: Call addSignature() and clear fields if no errors
-
-}
-
-signNowButton.addEventListener('click', validateForm);
-
-
-
-
-
-
-
-
 
 
 
